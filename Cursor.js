45696e3ecb3db0c919d155c43870125079327fa9
jacky chen <jackychen4@gmail.com>
2022-06-21 17:20:49 -0400

    commit on 'main'

---
+++ Cursor.js
@@ -0,0 +1,13 @@
+const cursorRound = document.querySelector(".cursor");
+const cursorPoint = document.querySelector(".cursor2");
+
+const cursorFunc = (e) =>{
+    const Y = e.clientY;
+    const X = e.clientX;
+
+    cursorRound.style.transform = `translate3d(${X}px, ${Y}px, 0)`;
+  
+    cursorPoint.style.transform = `translate3d(${X}px, ${Y}px, 0)`;
+}
+
+window.addEventListener('mousemove', cursorFunc);
