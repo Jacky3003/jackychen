fe8798c82e7c746351fdb8a660642b06910495b7
jacky chen <jackychen4@gmail.com>
2022-10-07 20:03:57 -0400

    Disabled Cursor and Added Fade in Effect

--- Cursor.js
+++ Cursor.js
@@ -1,13 +1,13 @@
-const cursorRound = document.querySelector(".cursor");
-const cursorPoint = document.querySelector(".cursor2");
+// const cursorRound = document.querySelector(".cursor");
+// const cursorPoint = document.querySelector(".cursor2");
 
-const cursorFunc = (e) =>{
-    const Y = e.clientY;
-    const X = e.clientX;
+// const cursorFunc = (e) =>{
+//     const Y = e.clientY;
+//     const X = e.clientX;
 
-    cursorRound.style.transform = `translate3d(${X}px, ${Y}px, 0)`;
+//     cursorRound.style.transform = `translate3d(${X}px, ${Y}px, 0)`;
   
-    cursorPoint.style.transform = `translate3d(${X}px, ${Y}px, 0)`;
-}
+//     cursorPoint.style.transform = `translate3d(${X}px, ${Y}px, 0)`;
+// }
 
-window.addEventListener('mousemove', cursorFunc);
+// window.addEventListener('mousemove', cursorFunc);
